118f1bd0011fb8af63b87b213fd54146cfaef79a
nayan5565 <nayan5565@gmail.com>
2017-07-13 02:57:52 +0600

    use checkbox

--- app/src/main/java/com/techienerd/questiongame/activity/CheckSystemActivity.java
+++ app/src/main/java/com/techienerd/questiongame/activity/CheckSystemActivity.java
@@ -4,8 +4,11 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -28,6 +31,9 @@ public class CheckSystemActivity extends AppCompatActivity implements View.OnCli
     private Button btnNext;
     private int pos;
     private TextView txtQues;
+    private CheckBox ch1, ch2, ch3, ch4;
+    private boolean isTrue;
+    LinearLayout layOption;
 
     private static CheckSystemActivity instance;
 
@@ -39,22 +45,34 @@ public class CheckSystemActivity extends AppCompatActivity implements View.OnCli
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.check_system_activity);
         instance = this;
         init();
         generate();
         prepareDisplay();
+//        setOptionWithCheckBox();
+        prepareOptionView();
     }
 
     private void init() {
+        ch1 = (CheckBox) findViewById(R.id.checkBox1);
+        ch2 = (CheckBox) findViewById(R.id.checkBox2);
+        ch3 = (CheckBox) findViewById(R.id.checkBox3);
+        ch4 = (CheckBox) findViewById(R.id.checkBox4);
+        ch1.setOnClickListener(this);
+        ch2.setOnClickListener(this);
+        ch3.setOnClickListener(this);
+        ch4.setOnClickListener(this);
         txtCount = (TextView) findViewById(R.id.txtCount);
         btnNext = (Button) findViewById(R.id.btnNext);
         btnNext.setOnClickListener(this);
         txtQues = (TextView) findViewById(R.id.tct);
         mOption = new MOption();
         mQuestion = new MQuestion();
-        optionArrayList = new ArrayList<>();
+//        optionArrayList = new ArrayList<>();
         questionArrayList = new ArrayList<>();
+
+        layOption= (LinearLayout) findViewById(R.id.layOption);
 
     }
 
@@ -65,19 +83,37 @@ public class CheckSystemActivity extends AppCompatActivity implements View.OnCli
             return;
         } else {
             txtQues.setText(questionArrayList.get(pos).getQues());
-            pos++;
+
         }
 //        txtCount.setText(QuestionAdapter.ca + " : " + QuestionAdapter.wa);
     }
 
-    public void setCount(CharSequence text) {
-        txtCount.setText(text);
+    void prepareOptionView(){
+        layOption.removeAllViews();
+        for (int i=0;i<questionArrayList.get(pos).getList().size();i++){
+            addCheckbox(questionArrayList.get(pos).getList().get(i).getItem()+"",i);
+        }
+
+    }
+    public void setOptionWithCheckBox() {
+        ch1.setText(questionArrayList.get(pos).getList().get(0).getItem() + "");
+        ch2.setText(questionArrayList.get(pos).getList().get(1).getItem() + "");
+        ch3.setText(questionArrayList.get(pos).getList().get(2).getItem() + "");
+        ch4.setText(questionArrayList.get(pos).getList().get(3).getItem() + "");
+//            ch1.setText(optionArrayList.get(0).getTag() + "");
+//            ch2.setText(optionArrayList.get(1).getTag() + "");
+//            ch3.setText(optionArrayList.get(2).getTag() + "");
+//            ch4.setText(optionArrayList.get(3).getTag() + "");
+
     }
 
     private void generate() {
+        questionArrayList = new ArrayList<>();
         optionArrayList = new ArrayList<>();
         mQuestion = new MQuestion();
+
         mQuestion.setQues("2+2");
+
         mOption = new MOption();
         mOption.setItem(2);
         mOption.setTag(0);
@@ -105,6 +141,8 @@ public class CheckSystemActivity extends AppCompatActivity implements View.OnCli
         mQuestion.setList(optionArrayList);
 
         questionArrayList.add(mQuestion);
+
+        // 2
 
         optionArrayList = new ArrayList<>();
         mQuestion = new MQuestion();
@@ -138,6 +176,7 @@ public class CheckSystemActivity extends AppCompatActivity implements View.OnCli
 
         questionArrayList.add(mQuestion);
 
+        //3
         optionArrayList = new ArrayList<>();
         mQuestion = new MQuestion();
         mQuestion.setQues("2+5");
@@ -170,6 +209,7 @@ public class CheckSystemActivity extends AppCompatActivity implements View.OnCli
 
         questionArrayList.add(mQuestion);
 
+        //4
         optionArrayList = new ArrayList<>();
         mQuestion = new MQuestion();
         mQuestion.setQues("7+2");
@@ -236,8 +276,97 @@ public class CheckSystemActivity extends AppCompatActivity implements View.OnCli
 
     }
 
+    void addCheckbox(String text, final int id){
+
+        CheckBox checkBox=new CheckBox(this);
+        checkBox.setText(text);
+        checkBox.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (questionArrayList.get(pos).getList().get(id).getTag() == 1) {
+                    Toast.makeText(CheckSystemActivity.this, "correct", Toast.LENGTH_SHORT).show();
+                } else {
+                    Toast.makeText(CheckSystemActivity.this, "wrong", Toast.LENGTH_SHORT).show();
+                }
+                for (int i = 0; i < layOption.getChildCount(); i++) {
+                    layOption.getChildAt(i).setClickable(false);
+                }
+
+
+            }
+        });
+
+
+        layOption.addView(checkBox);
+    }
+
     @Override
     public void onClick(View v) {
-        prepareDisplay();
+        int cId=v.getId();
+        if (v.getId() == R.id.btnNext) {
+            pos++;
+
+            prepareDisplay();
+            prepareOptionView();
+            for (int i = 0; i < layOption.getChildCount(); i++) {
+                layOption.getChildAt(i).setClickable(true);
+            }
+//            findViewById(100).setClickable(true);
+//            ch1.setClickable(true);
+//            ch2.setClickable(true);
+//            ch3.setClickable(true);
+//            ch4.setClickable(true);
+//            ch1.setChecked(false);
+//            ch2.setChecked(false);
+//            ch3.setChecked(false);
+//            ch4.setChecked(false);
+//            prepareDisplay();
+//            setOptionWithCheckBox();
+//            isTrue=false;
+
+        }
+        Log.e("TEST",cId+"");
+//        if (cId == 100) {
+//            isTrue = true;
+//            if (questionArrayList.get(pos).getList().get(0).getTag() == 1) {
+//                Toast.makeText(this, "correct", Toast.LENGTH_SHORT).show();
+//            } else {
+//                Toast.makeText(this, "wrong", Toast.LENGTH_SHORT).show();
+//            }
+//        }
+//        if (cId==101) {
+//            isTrue = true;
+//            if (questionArrayList.get(pos).getList().get(1).getTag() == 1) {
+//                Toast.makeText(this, "correct", Toast.LENGTH_SHORT).show();
+//            } else {
+//                Toast.makeText(this, "wrong", Toast.LENGTH_SHORT).show();
+//            }
+//        }
+//        if (cId==102) {
+//            isTrue = true;
+//            if (questionArrayList.get(pos).getList().get(2).getTag() == 1) {
+//                Toast.makeText(this, "correct", Toast.LENGTH_SHORT).show();
+//            } else {
+//                Toast.makeText(this, "wrong", Toast.LENGTH_SHORT).show();
+//            }
+//        }
+//        if (cId==103) {
+//            isTrue = true;
+//            if (questionArrayList.get(pos).getList().get(3).getTag() == 1) {
+//                Toast.makeText(this, "correct", Toast.LENGTH_SHORT).show();
+//            } else {
+//                Toast.makeText(this, "wrong", Toast.LENGTH_SHORT).show();
+//            }
+//        }
+
+
+//            findViewById(100).setClickable(false);
+//             findViewById(101).setClickable(false);
+//             findViewById(102).setClickable(false);
+//             findViewById(103).setClickable(false);
+
+
+
+
     }
 }
