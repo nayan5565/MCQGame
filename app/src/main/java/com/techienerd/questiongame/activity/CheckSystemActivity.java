1c69b215a8d78679626f1b840573319909814734
nayan5565 <nayan5565@gmail.com>
2017-07-18 22:59:09 +0600

    auto change

--- app/src/main/java/com/techienerd/questiongame/activity/CheckSystemActivity.java
+++ app/src/main/java/com/techienerd/questiongame/activity/CheckSystemActivity.java
@@ -4,6 +4,7 @@ import android.app.Dialog;
 import android.graphics.Color;
 import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
@@ -32,16 +33,17 @@ public class CheckSystemActivity extends AppCompatActivity implements View.OnCli
 //    MQuestion mQuestion;
 //    MOption mOption;
     private TextView txtCount;
-    private Button btnNext,btnStatistics;
+    private Button btnNext, btnStatistics;
     private int pos;
-    private TextView txtQues, txtType, txtWrong;
+    private TextView txtQues, txtType, txtWrong, txtOptionChanged;
     private CheckBox ch1, ch2, ch3, ch4;
     private boolean isTrue;
     private LinearLayout layOption;
     private int correct, wrong;
-    private int index;
+    private int index, count=10;
     private int parentId;
     private int bestScore;
+    private Handler handler;
     private MScore mScore;
     private DatabaseHelper db;
 //    SparseBooleanArray checked = listView.getCheckedItemPositions();
@@ -59,6 +61,8 @@ public class CheckSystemActivity extends AppCompatActivity implements View.OnCli
         prepareDisplay();
 //        setOptionWithCheckBox();
 //        prepareOptionView();
+        change();
+
 
     }
 
@@ -67,7 +71,7 @@ public class CheckSystemActivity extends AppCompatActivity implements View.OnCli
         index = getIntent().getIntExtra("index", 0);
         Global.parentId = getIntent().getIntExtra("parentId", 0);
         parentId = Global.parentId;
-        Log.e("id"," is "+parentId);
+        Log.e("id", " is " + parentId);
 
         ch1 = (CheckBox) findViewById(R.id.checkBox1);
         ch2 = (CheckBox) findViewById(R.id.checkBox2);
@@ -80,6 +84,7 @@ public class CheckSystemActivity extends AppCompatActivity implements View.OnCli
         txtCount = (TextView) findViewById(R.id.txtCount);
         txtType = (TextView) findViewById(R.id.txtType);
         txtWrong = (TextView) findViewById(R.id.txtWrong);
+        txtOptionChanged = (TextView) findViewById(R.id.txtOptionChange);
         btnNext = (Button) findViewById(R.id.btnNext);
         btnNext.setOnClickListener(this);
         txtQues = (TextView) findViewById(R.id.tct);
@@ -89,6 +94,7 @@ public class CheckSystemActivity extends AppCompatActivity implements View.OnCli
         txtType.setText(MainActivity.getInstance().allQuestionArrayList.get(index).getType());
         db = new DatabaseHelper(this);
         layOption = (LinearLayout) findViewById(R.id.layOption);
+        handler = new Handler();
 
     }
 
@@ -103,14 +109,14 @@ public class CheckSystemActivity extends AppCompatActivity implements View.OnCli
             Toast.makeText(this, "level completed", Toast.LENGTH_SHORT).show();
             final Dialog dialog = new Dialog(this);
             dialog.setCancelable(false);
-            dialog.requestWindowFeature(Window.FEATURE_NO_TITLE);
+//            dialog.requestWindowFeature(Window.FEATURE_NO_TITLE);
             dialog.setContentView(R.layout.dia_game_over);
             dialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
             Button btnOk = (Button) dialog.findViewById(R.id.btnOK);
             final TextView txtMark = (TextView) dialog.findViewById(R.id.txtScore);
             TextView txtBestScore = (TextView) dialog.findViewById(R.id.txtBestScore);
 //            int bp = db.getBestScores(parentId);
-            bestScore =  db.getBestScores(parentId);
+            bestScore = db.getBestScores(parentId);
             int score = correct * (100 / MainActivity.getInstance().allQuestionArrayList.get(index).getQuestionArrayList().size());
             if (score > bestScore) {
                 bestScore = score;
@@ -130,18 +136,20 @@ public class CheckSystemActivity extends AppCompatActivity implements View.OnCli
             btnOk.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View v) {
+                    pos = 0;
                     correct = 0;
                     wrong = 0;
                     txtCount.setText(correct + " : ");
                     txtWrong.setText(wrong + "");
                     Collections.shuffle(MainActivity.getInstance().allQuestionArrayList.get(index).getQuestionArrayList());
-                    prepareDisplay();
+//                    prepareDisplay();
+                    change();
                     dialog.dismiss();
                 }
             });
             dialog.show();
 //            layOption.removeAllViews();
-            pos = 0;
+
             return;
         } else {
             Log.e("step", "two");
@@ -567,6 +575,41 @@ public class CheckSystemActivity extends AppCompatActivity implements View.OnCli
 //            return;
 //        }
         layOption.addView(checkBox);
+    }
+
+    public void change() {
+
+        count--;
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                if (count == -1) {
+                    pos++;
+                    wrong++;
+                    Log.e("position"," is "+pos);
+                    prepareDisplay();
+                    for (int i = 0; i < layOption.getChildCount(); i++) {
+                        layOption.getChildAt(i).setClickable(true);
+                    }
+                    count = 10;
+                    txtWrong.setText(wrong + "");
+
+                }
+                txtOptionChanged.setText(count + "");
+                if (pos >= MainActivity.getInstance().allQuestionArrayList.get(index).getQuestionArrayList().size()) {
+                    Log.e("position"," if"+pos);
+                    Log.e("position"," size"+MainActivity.getInstance().allQuestionArrayList.get(index).getQuestionArrayList().size());
+                    return;
+                } else {
+                    change();
+                }
+
+
+
+
+            }
+        }, 500);
+
     }
 
 
