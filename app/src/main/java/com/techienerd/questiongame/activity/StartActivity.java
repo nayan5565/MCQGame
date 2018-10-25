54f3d57e951d92431eeeb4f9153c4c936dca1666
nayan5565 <nayan5565@gmail.com>
2018-10-25 18:11:57 +0600

    tried marquee but not work

--- app/src/main/java/com/techienerd/questiongame/activity/StartActivity.java
+++ app/src/main/java/com/techienerd/questiongame/activity/StartActivity.java
@@ -1,23 +1,32 @@
 package com.techienerd.questiongame.activity;
 
 import android.content.Intent;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 import com.techienerd.questiongame.R;
 
 public class StartActivity extends AppCompatActivity implements View.OnClickListener {
 
     private Button btnStartPlay;
+    private TextView tvMarq;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_start);
         btnStartPlay = findViewById(R.id.btnStartPlay);
+        tvMarq = findViewById(R.id.tvMarq);
         btnStartPlay.setOnClickListener(this);
+//        tvMarq.setEllipsize(TextUtils.TruncateAt.MARQUEE);
+//        tvMarq.setSingleLine(true);
+        tvMarq.setSelected(true);
+
 
     }
 
